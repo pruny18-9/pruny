41cf73213b34df885ff61e1ed5e1c5cf9d876308
Hurlang <iamseongmin@naver.com>
2024-01-18 01:22:14 +0900

    @Hurlang modified buttons

--- icebox_fe/icebox_fe/src/App.js
+++ icebox_fe/icebox_fe/src/App.js
@@ -6,20 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello jenkins~!!!
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <button>냉장고 보기</button>
         <button>음식 넣기</button>
         <button>음식 버리기</button>
